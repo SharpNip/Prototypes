ef27e77561c47b7226400261e1930d5ee4829560
NickP <nick-potvin@outlook.com>
2018-06-23 13:58:08 -0400

    Start of project

---
+++ TaskManager/TaskManager/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class TaskManager
+    {
+
+
+        private static TaskManager mInstance;
+        public static TaskManager Instance
+        {
+            get { return mInstance ?? new TaskManager(); }
+        }
+
+        public TaskManager()
+        {
+
+        }
+
+
+
+
+
+
+
+    }
+}
