d2580b084d3e8a52a1898ce28e215ab70397e827
NickP <nick-potvin@outlook.com>
2018-11-17 19:13:36 -0500

    Updated timer to work. Implemented base for necessary stuff.

---
+++ SeatingReminder/Libs/ViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libs
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        private Dictionary<string, object> Properties = new Dictionary<string, object>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected T Get<T>([CallerMemberName] string name = "")
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                return Properties.ContainsKey(name) ? (T)Properties[name] : default(T);
+            }
+
+            return default(T);
+        }
+
+        protected void Set(object newValue, [CallerMemberName] string name = "")
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                if (!Properties.ContainsKey(name))
+                {
+                    Properties.Add(name, newValue);
+                }
+                else
+                {
+                    Properties[name] = newValue;
+                }
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
--- SeatingReminder/SeatingReminder/ViewModel/WindowViewModel.cs
+++ SeatingReminder/SeatingReminder/ViewModel/WindowViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using Libs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
 namespace SeatingReminder
 {
-    public sealed class WindowViewModel
+    public sealed class WindowViewModel : ViewModelBase
     {
+        private System.Timers.Timer timer;
+
+        private long timeElapsed = 0;
+        private long startTime = 0;
+        private long stoppedTime;
+
         public WindowViewModel()
         {
             InitCommands();
@@ -18,13 +27,97 @@ namespace SeatingReminder
 
         private void InitCommands()
         {
-            OnButtonClicked = new CommandHandler((param) => ButtonClicked(param), true);
-            
+            OnButtonClicked = new CommandHandler((param) => ButtonClicked(param), true);            
         }
 
         private void ButtonClicked(string param)
         {
-            Hours = "Test";
+            switch (param)
+            {
+                case "Start":
+                {
+                    StartClock();
+                    break;
+                }
+
+                case "Pause":
+                {
+                    TogglePauseClock();
+                    break;
+                }
+
+                case "Reset":
+                {
+                    ResetClock();
+                    break;
+                }
+
+                default:
+                {
+                    break;
+                }
+            }
+        }
+
+        private void TogglePauseClock()
+        {
+            if (timer != null)
+            {
+                timer.Enabled = !timer.Enabled;
+
+                if (timer.Enabled)
+                {
+                    startTime += (DateTime.UtcNow.Ticks - stoppedTime);
+                }
+                else
+                {
+                    stoppedTime = DateTime.UtcNow.Ticks;
+                }
+            }
+        }
+
+        private void StartClock()
+        {
+            if (timer == null)
+            {
+                timeElapsed = 0;
+
+                startTime = DateTime.UtcNow.Ticks;
+
+                timer = new System.Timers.Timer(1d)
+                {
+                    AutoReset = true,
+                    Enabled = true,
+                };
+
+                timer.Elapsed += (a, b) =>
+                {
+                    timeElapsed = DateTime.UtcNow.Ticks - startTime;
+                    MilliSeconds = (timeElapsed % 10000000).ToString();
+                    Seconds      = ((timeElapsed / 10000000) % 60).ToString("00");
+                    Minutes      = ((timeElapsed / 600000000) % 60).ToString("00");
+                    Hours        = ((timeElapsed / 36000000000) % 24).ToString("00");
+                };
+            }
+            else
+            {
+                TogglePauseClock();
+            }
+        }
+
+        private void ResetClock()
+        {
+            if (timer != null)
+            {
+                timer.Close();
+                timer.Dispose();
+                timer = null;
+
+                MilliSeconds = "000";
+                Seconds      = "00";
+                Minutes      = "00";
+                Hours        = "00";
+            }
         }
 
         public ICommand OnButtonClicked
@@ -33,9 +126,28 @@ namespace SeatingReminder
             private set;
         }
 
-        public string Hours   { get; private set; }
-        public string Minutes { get; private set; }
-        public string Seconds { get; private set; }
+        public string Hours
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
 
+        public string Minutes
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string Seconds
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string MilliSeconds
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
     }
 }
